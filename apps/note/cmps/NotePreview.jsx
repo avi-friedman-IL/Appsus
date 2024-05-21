83845ea6b6045999dd97b6c3d64481d65d88faba
Yana <yana102hanan@gmail.com>
2024-05-21 18:47:46 +0300

    Add initial preview

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -1,7 +1,24 @@
-export function NotePreview() {
+export function NotePreview({ note }) {
   return (
-    <div>
-      <span>make a note</span>
+    <div className="note-card" style={note.style}>
+      <p>{note.info.txt ? note.info.txt : ""}</p>
+      <p>{note.info.title ? note.info.title : ""}</p>
+
+      <ul>
+        <span>To Do: </span>
+        {note.info.todos
+          ? note.info.todos.map((todo, index) => (
+              <li key={index}>{todo.txt}</li>
+            ))
+          : ""}
+      </ul>
+
+      <div className="icons">
+        <span>✖️</span>
+        <span>
+          <img src="../assets/img/delete_24dp_FILL0_wght400_GRAD0_opsz24.png" />
+        </span>
+      </div>
     </div>
   );
 }
