675cbeabc5c822565ca469b50f9c188529a52d46
Yana <yana102hanan@gmail.com>
2024-05-21 15:08:55 +0300

    Add notes array in note.servoce

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,54 @@
 // note service
+import { utilService } from '../../email/services/util-service.js'
+import { storageService } from '../../../services/storage.service.js'
+
+
+var keepsDB = []
+const KEEPS_KEY = 'keeps'
+
+const notes = [
+    {
+        id: 'n101',
+        createdAt: 1112222,
+        type: 'NoteTxt',
+        isPinned: true,
+        style: {
+            backgroundColor: '#00d'
+        },
+        info: {
+            txt: 'Fullstack Me Baby!'
+        }
+    },
+    {
+        id: 'n102',
+        type: 'NoteImg',
+        isPinned: false,
+        info: {
+            url: 'http://some-img/me',
+            title: 'Bobi and Me'
+        },
+        style: {
+            backgroundColor: '#00d'
+        }
+    },
+    {
+        id: 'n103',
+        type: 'NoteTodos',
+        isPinned: false,
+        info: {
+            title: 'Get my stuff together',
+            todos: [
+                { txt: 'Driving license', doneAt: null },
+                { txt: 'Coding power', doneAt: 187111111 }
+            ]
+        }
+    }
+]
+
+function createNote() { }
+
+function deleteNote() { }
+
+function updateNote() {
+
+}
