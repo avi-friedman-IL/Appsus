12b18a07f46ef8bf04884a616553804471105ec8
Yana <yana102hanan@gmail.com>
2024-05-22 20:30:15 +0300

    Fix function names

--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,6 +1,7 @@
 const { useState, useEffect } = React;
 import { notesService } from "../services/note.service.js";
 import { NoteList } from "../cmps/NoteList.jsx";
+import { Form } from "../cmps/Form.jsx";
 
 export function NoteIndex() {
   const [notes, setNotes] = useState([]);
@@ -18,7 +19,7 @@ export function NoteIndex() {
   }
 
   function handleAddNote(note) {
-    notesService.addNote(note).then(() => {
+    notesService.saveNote(note).then(() => {
       setNotes((prevNotes) => [...prevNotes, note]);
     });
   }
@@ -28,58 +29,5 @@ export function NoteIndex() {
       <Form onAddNote={handleAddNote} />
       <NoteList notes={notes} onRemoveNote={handleRemoveNote} />
     </section>
-  );
-}
-
-function Form({ onAddNote }) {
-  const [infoTxt, setInfoTxt] = useState("");
-
-  function handleSubmit(ev) {
-    ev.preventDefault();
-
-    const newNote = {
-      id: "",
-      createdAt: Date.now(),
-      type: "NoteTxt",
-      isPinned: false,
-      style: {
-        backgroundColor: "rgb(173,173,215)",
-      },
-      info: {
-        txt: infoTxt,
-      },
-    };
-
-    onAddNote(newNote);
-    setInfoTxt("");
-  }
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <div>
-        <input
-          id="add-note"
-          type="text"
-          placeholder="Add a Note"
-          value={infoTxt}
-          onChange={(ev) => setInfoTxt(ev.target.value)}
-        />
-
-        <button className="btn img-btn">
-          <i className="fa-solid fa-image"></i>
-        </button>
-
-        <button className="btn video-btn">
-          <i className="fa-brands fa-youtube"></i>
-        </button>
-
-        <button className="btn audio-btn">
-          <i className="fa-solid fa-volume-high"></i>
-        </button>
-        <button className="btn list-btn">
-          <i className="fa-solid fa-list-ul"></i>
-        </button>
-      </div>
-    </form>
   );
 }
