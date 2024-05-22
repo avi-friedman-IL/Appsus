f174a12d0a9c28125fcf2c7734663183252278c5
avi friedman <avi551650@gmail.com>
2024-05-22 14:11:36 +0300

    Create mailList cmp

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,3 +1,12 @@
-export function MailList() {
-    return <div>Mail list</div>
+import { MailPreview } from "./MailPreview.jsx";
+
+export function MailList({ mails, onRemove }) {
+    return <ul className="mail-list">
+        {mails.map(mail =>
+            <li key={mail.id}>
+                {<MailPreview mail={mail} />}
+                <button className="fa fa-remove-mail remove-mail" onClick={() => onRemove(mail.id)}></button>
+            </li>
+        )}
+    </ul>
 }
