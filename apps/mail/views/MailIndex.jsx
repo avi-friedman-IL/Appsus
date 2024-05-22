144219a9de2f029f397d3c0f3d410b62a71568ff
avi friedman <avi551650@gmail.com>
2024-05-22 14:12:57 +0300

    Creatae mailIndex cmp

--- apps/mail/views/MailIndex.jsx
+++ apps/mail/views/MailIndex.jsx
@@ -1,5 +1,38 @@
+const { useState, useEffect } = React
+const { Link, useSearchParams } = ReactRouterDOM
+
+
+import { MailFilter } from "../cmps/MailFilter.jsx"
+import { MailList } from "../cmps/MailList.jsx"
+import { mailService } from "../services/mail.service.js"
 
 export function MailIndex() {
-    return <div>mail app</div>
+
+    const [mails, setMails] = useState([])
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [filterBy, setFilterBy] = useState(mailService.getFilterFromSearchParams(searchParams))
+
+
+    function removeMail(mailId) {
+        mailService.remove(mailId)
+        .then(() => {
+            setMails(prevMails => prevMails.filter(mail => mail.id !== mailId))
+        })
+    }
+
+    useEffect(() => {
+        setSearchParams(filterBy)
+        mailService.query(filterBy)
+            .then(mails => setMails(mails))
+    }, [filterBy])
+
+    function onSetFilterBy(newFilter) {
+        setFilterBy({...newFilter})
+    }
+
+    return <section className="mail-index">
+        {<MailFilter filterBy={filterBy} onFilter={onSetFilterBy}/>}
+        {<MailList mails={mails} onRemove={removeMail}/>}
+    </section>
 }
 
