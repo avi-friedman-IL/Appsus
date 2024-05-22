4062608fed254b7c6abb3bf9aef632dc39145437
Yana <yana102hanan@gmail.com>
2024-05-22 20:29:00 +0300

    Change from div to ul

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -2,14 +2,12 @@ import { NotePreview } from "../../../apps/note/cmps/NotePreview.jsx";
 
 export function NoteList({ notes, onRemoveNote }) {
   return (
-    <div>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id}>
-            <NotePreview note={note} onRemoveNote={onRemoveNote} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id} className="note-card" style={note.style}>
+          <NotePreview note={note} onRemoveNote={onRemoveNote} />
+        </li>
+      ))}
+    </ul>
   );
 }
