847cc7868f10216d952b79dfffc999ef02d6d07e
Yana <yana102hanan@gmail.com>
2024-05-25 09:46:09 +0300

    Fix colors vars

--- apps/note/cmps/ColorInput.jsx
+++ apps/note/cmps/ColorInput.jsx
@@ -1,19 +1,12 @@
 export function ColorInput({ onSetBgc }) {
-  // console.log("backgroundColor:", backgroundColor);
-
   const colors = [
-    "#faafa8",
-    "#f39f76",
-    "#e2f6d3",
-    "#d4e4ed",
-    "#d3bfdb",
-    "#f6e2dd",
+    "var(--keep-note-bgs-1)",
+    "var(--keep-note-bgs-2)",
+    "var(--keep-note-bgs-3)",
+    "var(--keep-note-bgs-4)",
+    "var(--keep-note-bgs-5)",
+    "var(--keep-note-bgs-6)",
   ];
-
-  // function onSetColor(color) {
-  //   const newStyle = { backgroundColor: color };
-  //   onSetFooterStyle(newStyle);
-  // }
 
   return (
     <div className="color-container">
