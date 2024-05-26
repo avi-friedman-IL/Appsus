28531d291d1c23cfa2e262b6bf78bdc51748f943
Yana <yana102hanan@gmail.com>
2024-05-27 01:19:18 +0300

    Add titles

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -31,25 +31,37 @@ function IconsModal({ onToggleModal }) {
   return (
     <section className="icons-modal">
       <NavLink to="/">
-        <img src="../assets/img/home.png" alt="home" onClick={onToggleModal} />
+        <img
+          src="../assets/img/home.png"
+          alt="home"
+          title="home"
+          onClick={onToggleModal}
+        />
       </NavLink>
 
       <NavLink to="/about">
         <img
           src="../assets/img/about.png"
           alt="about"
+          title="about"
           onClick={onToggleModal}
         />
       </NavLink>
 
       <NavLink to="/mail">
-        <img src="../assets/img/mail.png" alt="mail" onClick={onToggleModal} />
+        <img
+          src="../assets/img/mail.png"
+          alt="mail"
+          title="mail"
+          onClick={onToggleModal}
+        />
       </NavLink>
 
       <NavLink to="/note">
         <img
           src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
           alt="keep"
+          title="mail"
           onClick={onToggleModal}
         />
       </NavLink>
