dad93d706e71bf82c993f14fce35e9b480300411
avi friedman <avi551650@gmail.com>
2024-07-10 23:21:41 +0300

    More styling 2

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,31 +1,29 @@
 const { Link } = ReactRouterDOM
 
-import { utilService } from "../../../services/util.service.js";
-import { MailCompose } from "../views/MailCompose.jsx";
-import { MailPreview } from "./MailPreview.jsx";
-import { MailStarred } from "./MailStarred.jsx";
+import { utilService } from '../../../services/util.service.js'
+import { MailCompose } from '../views/MailCompose.jsx'
+import { MailPreview } from './MailPreview.jsx'
+import { MailStarred } from './MailStarred.jsx'
 
 export function MailList({ mails, onRemove, onToggleRead, close }) {
+    return (
+        <ul className="mail-list">
+            {mails.map(mail => (
+                <li className={mail.isRead ? 'read' : ''} key={mail.id}>
+                    {<MailStarred mail={mail} />}
+                    <Link to={`/mail/${mail.id}`}>{<MailPreview mail={mail} />}</Link>
 
-    return <ul className="mail-list">
-        {mails.map(mail =>
-            <li className={mail.isRead ? 'read' : ''} key={mail.id} onClick={() => mail.isRead = true}>
+                    <div className="mail-list-actions">
+                        <button className={mail.isRead ? 'fa fa-unread-mail unread-mail' : 'fa fa-read-mail read-mail'} onClick={() => onToggleRead(mail.id)}></button>
+                        <button className="fa fa-remove-mail remove-mail" onClick={() => onRemove(mail.id)}></button>
+                    </div>
 
-                {<MailStarred mail={mail} />}
-                <Link to={`/mail/${mail.id}`}>{<MailPreview mail={mail} />}</Link>
-
-                <div className="mail-list-actions">
-                    <button className={mail.isRead ? 'fa fa-unread-mail unread-mail' : 'fa fa-read-mail read-mail'} onClick={() => onToggleRead(mail.id)}></button>
-                    <button className="fa fa-remove-mail remove-mail" onClick={() => onRemove(mail.id)}></button>
-                </div>
-
-                <p className="sent-at">
-                    {`${utilService.getMonthName(new Date(mail.sentAt)).substring(0, 3)} 
+                    <p className="sent-at">
+                        {`${utilService.getMonthName(new Date(mail.sentAt)).substring(0, 3)} 
                     ${new Date(mail.sentAt).getDate()} ${new Date(mail.sentAt).getFullYear()}`}
-                </p>
-
-            </li>
-        )}
-    </ul>
-
+                    </p>
+                </li>
+            ))}
+        </ul>
+    )
 }
--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,6 +1,6 @@
 export function MailPreview({ mail }) {
     return (
-        <section className="mail-preview">
+        <section className="mail-preview" onClick={() => mail.isRead = true}>
             <h2>{mail.from === 'momo@momo.com' ? 'Me' : 'User'}</h2>
             <p>
                 <span>{mail.subject ? mail.subject : '(No Subject)'} - </span>
